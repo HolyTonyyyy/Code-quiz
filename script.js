3ebe1d45bc32e4873826cd2ee0680313a052ca96
Anthony Bradford <agb2152001@gmail.com>
2023-04-27 12:43:20 -0400

    sencond-commit

---
+++ script.js
@@ -0,0 +1,54 @@
+//-- Start Button 
+
+var startButton = document.querySelector(".start")
+
+startButton.addEventListener("click", function () {
+    window.prompt("Q1 .Javascript is an _______ language? (Object-Oriented, Object-Based Procedural, Procadeural, None")
+        if (window.prompt = "Object-Oriented") {
+            console.log("Correct");
+
+            
+        }
+    
+    
+});
+
+//--This is the Timer below
+
+
+
+// // Set the number of seconds for the timer
+// var seconds = 60;
+
+// // Convert seconds to milliseconds
+// var milliseconds = seconds * 1000;
+
+// // Get the HTML element where the timer will be displayed
+// var timerDisplay = document.getElementById('.start');
+
+// // Set the interval for the timer (1000 milliseconds = 1 second)
+// var countdownInterval = setInterval(function() {
+//   // Decrease the number of seconds by 1
+//   seconds--;
+
+//   // Update the timer display with the new number of seconds
+//   timerDisplay.innerHTML = seconds;
+
+//   // If the timer has reached 0 seconds, stop the timer and show a message
+//   if (seconds === 0) {
+//     clearInterval(countdownInterval);
+//     timerDisplay.innerHTML = "Time's up!";
+//   }
+// }, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
